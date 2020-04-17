90fbc9ee818fabee06c1c074447538ebb80761e5
Paulo Negrao <paulo.negrao@ymail.com>
2020-04-16 19:52:56 -0700

    update video

--- index.php
+++ index.php
@@ -12,15 +12,15 @@ Project1 PHP Includes
 		<source src="videos/capoeira6.mp4" type="video/mp4">
 	</video>
 	<video id="home_video2" muted name="media">
-		<source src="videos/capoeira6.mp4" type="video/mp4">
+		<source src="videos/capoeira1.mp4" type="video/mp4">
 	</video>
 </div>
 <section>
 	<p>Borned and raised in Brazil, Capoeira is a cultural expression and a mix of martial art, coporal language and rythms. Characterized by their agile and harmonious movements, percusion instruments and achorbatic skills, the Capoeira practitioners use the movements of their entire body to atack and defend, in a dynamic kind of fighting & dancing performance inside the <em>roda</em>.</p>
-	<p>The main instrument used in capoeira the is <em>berimbau</em>. It dictates the rhythm and the style of the game, giving a characteristic sound to the capoeira. The berimbau is basically made up of a piece of wire, a stick and a gourd. The rhythms can vary from slow, cadenced, and very fast beats.</p>
+	<p>The main instrument used in capoeira is the <em>berimbau</em>. It dictates the rhythm and the style of the game, giving a very characteristic sound to the capoeira performances. The berimbau is basically made up of a piece of wire, a stick and a gourd. The rhythms can vary from slow, cadenced, and very fast beats.</p>
 	<p><em>Ginga</em> is the movement of the whole body in a rhythmic way, keeping the body relaxed, shifting the center of gravity of the body constantly and keeping alert for dodge, attack and counterattack movements.
 
-	There are several styles of capoeira, with 3 main ones:
+	There are different styles of capoeira, with 3 main ones:
 	<dl>
 		<dt>Angola</dt>
 		<dd>Is the oldest, from the time of slavery. Its main characteristics are: the blows are thrown close to the ground, the musical rhythm is slower and there is a lot of malice. During the roda the participants do not clap, but sing.</dd>
